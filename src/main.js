00c7e55a8e103c72b1712e7acff0448cd0797f8d
Emerson Caique <emerson.caique@outlook.com>
2019-09-22 14:18:23 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Web3 from 'web3'
 
-Vue.config.productionTip = false
+(async () => {
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum)
+    await window.ethereum.enable()
+  } else if (window.web3) {
+    window.web3 = new Web3(window.web3.currentProvider)
+  } else {
+    window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+  }
+})()
 
 new Vue({
   render: h => h(App),
